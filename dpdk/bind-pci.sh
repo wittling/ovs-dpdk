96d20f5f41602de8ee5648daadfd4bf946ef009a
Wittling <mark.wittling@gmail.com>
2020-07-22 09:26:33 -0400

    updates

--- dpdk/bind-pci.sh
+++ dpdk/bind-pci.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # driverctl unset-override 0000:01:00.0
-#driverctl set-override 0000:01:00.0 vfio
+#driverctl set-override 0000:01:00.0 vfio-pci
 driverctl set-override 0000:01:00.0 uio_pci_generic
 driverctl set-override 0000:01:00.1 uio_pci_generic
 
--- dpdk/check-bindings.sh
+++ dpdk/check-bindings.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
 
 dpdk-devbind --status
-
-# driverctl unset-override 0000:01:00.0
-#driverctl set-override 0000:01:00.0 vfio
-#driverctl set-override 0000:01:00.0 uio_pci_generic
